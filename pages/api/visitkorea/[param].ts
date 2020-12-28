f306e1673ea1900279a1578a6d7589b676ca8215
quavious <leenowon97@gmail.com>
2020-12-28 15:34:30 +0900

    API ERROR TEST

--- pages/api/visitkorea/[param].ts
+++ pages/api/visitkorea/[param].ts
@@ -1,13 +1,17 @@
 import axios from 'axios'
 
 export default async (req, res) => {
+    let tempList = [] as string[]
+    let tempWord = "" as string
     try {
         const visitkoreaList = []
         const param:string = req.query.param
         const translateUrl = `https://translation.googleapis.com/language/translate/v2?q=${param}&target=ko&key=${process.env.GOOGLE_TRANSLATION}`
         const translateResp = await axios.post(translateUrl)
         const translateData:string = await translateResp.data.data.translations[0].translatedText
-        
+
+        tempWord = translateData
+
         let visitkoreaUrl = 'http://api.visitkorea.or.kr/openapi/service/rest/PhotoGalleryService/gallerySearchList'
         visitkoreaUrl += "?ServiceKey=" + process.env.VISIT_KOREA
         visitkoreaUrl += "&pageNo=1"
@@ -19,6 +23,9 @@ export default async (req, res) => {
         
         const visitkoreaResp = await axios.get(visitkoreaUrl)
         const visitkoreaData:any[] = await visitkoreaResp.data.response.body.items.item
+        
+        tempList = visitkoreaData
+        
         for(let i = 0; i < visitkoreaData.length; i++){
             visitkoreaList.push(await visitkoreaData[i].galWebImageUrl)
         }
@@ -29,7 +36,11 @@ export default async (req, res) => {
         console.error(err)
         res.json({
             "status": "fail",
-            "msg": "Server ERROR"
+            "msg": "Server ERROR",
+            "errorData": {
+                "list" : tempList,
+                "word": tempWord
+            }
         })
     }
 }
