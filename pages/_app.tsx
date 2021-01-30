d92e3d05278b0923951301d821b97d6c4fccd129
quavious <leenowon97@gmail.com>
2021-01-30 23:39:37 +0900

    Make Auth Asynchronous

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,28 +3,50 @@ import Head from 'next/head'
 import NavigationBar from '../components/navbar'
 import { useEffect, useState } from 'react'
 import axios from 'axios'
+import Router from "next/router";
 
 import {UserInfo} from '../interfaces/user/auth'
 
 function MyApp({ Component, pageProps }) {
   const [user, setUser] = useState<UserInfo>(null)
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
-    axios.post('/api/users/auth')
-    .then(resp => resp.data)
-    .then(async (data) => {
-        if(await data.status !== "OK") {
-            return;
-        }
-        const {username, email} = await data
-        setUser({
-          username: await username,
-          email: await email,
-        })
-    })
-    .catch(err => {
-        console.error(err)
-    })
+    (async () => {
+      axios.post('/api/users/auth')
+      .then(resp => resp.data)
+      .then(async (data) => {
+          if(await data.status !== "OK") {
+              return;
+          }
+          const {username, email} = await data
+          setUser({
+            username: await username,
+            email: await email,
+          })
+      })
+      .catch(err => {
+          console.error(err)
+      })
+    })()
   }, [])
+  useEffect(() => {
+    const start = () => {
+      console.log("start");
+      setLoading(true);
+    };
+    const end = () => {
+      console.log("findished");
+      setLoading(false);
+    };
+    Router.events.on("routeChangeStart", start);
+    Router.events.on("routeChangeComplete", end);
+    Router.events.on("routeChangeError", end);
+    return () => {
+      Router.events.off("routeChangeStart", start);
+      Router.events.off("routeChangeComplete", end);
+      Router.events.off("routeChangeError", end);
+    };
+  }, []);
 
   return ( 
     <>
